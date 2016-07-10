334f8951a628a43ae8318fa24a3615325b6482d8
shivangrai <shivangrai95@gmail.com>
2016-07-10 21:01:14 +0530

    Masking localhost configurations

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'test',
-			'username'  => 'root',
-			'password'  => 'luna',
+			'database'  => '****',
+			'username'  => '****',
+			'password'  => '****',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
